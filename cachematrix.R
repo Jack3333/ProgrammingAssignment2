fcb2ffab6b7cf154009ee9442d9dd54765753f4d
Jack3333 <jjf333@comcast.net>
2015-01-22 16:34:10 -0500

    Completed and tested version of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,25 +4,193 @@
 ## pair of functions enable one to retrieve the previously determined
 ## inverse from a cache.
 
-## This function provides
+## This function produces a list of objects to be passed to cacheSolve which
+## will in turn use the objects in the determination of the inverse of a
+## given matrix either by retrieval from a cache or direct calculation. 
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
       inv <- NULL
       set <- function(y) {
             x <<- y
             inv<<- NULL
       }
-      get <- function() X
-      setinv <- function(solve)   inv<<- solve
-      getinv - function() inv
+      get <- function() x
+      setinv <- function(inverse)   inv<<- inverse
+      getinv <- function() inv
       list (set = set, get = get,
             setinv = setinv,
             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve calculates the inverse of the matrix created above. 
+## It first checks to see if the inverse has already been calculated.
+## If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the matrix and sets the value
+## of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+              message("getting cached data")
+              return(inv)
+        }
+        data <-x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+               
+             
+              
+              
+      
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
